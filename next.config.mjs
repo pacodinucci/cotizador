f2c9f817a418bbfde72da0a228c2a795bc4c0630
pacodinucci <franciscoldinucci@gmail.com>
2025-02-14 16:12:33 -0300

    next config size limit

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,11 @@ const nextConfig = {
   experimental: {
     esmExternals: "loose",
   },
+  api: {
+    bodyParser: {
+      sizeLimit: "50mb", // Ajusta este valor según sea necesario
+    },
+  },
 };
 
 export default nextConfig;
